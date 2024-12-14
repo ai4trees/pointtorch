a74e0b6726e56234593a37cdd4420ec8ed47631c
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2024-12-14 15:38:02 +0100

    refactor pointtorch.operations.numpy.make_lanels_consecutive

--- test/operations/numpy/test_make_labels_consecutive.py
+++ test/operations/numpy/test_make_labels_consecutive.py
@@ -1,17 +1,64 @@
 """ Tests for pointtorch.operations.numpy.make_labels_consecutive. """
 
+from typing import Optional
+
 import numpy as np
+import pytest
 
 from pointtorch.operations.numpy import make_labels_consecutive
 
 
-class TestMakeLabelsConsecutive:  # pylint: disable=too-few-public-methods
-    """Tests for pointtorch.operations.numpy.make_labels_consecutive."""
+class TestMakeLabelsConsecutive:
+    """Tests for pointtorch.operations.numpy.make_labels_consecutive"""
 
-    def test_make_labels_consecutive(self):
-        labels = np.array([10, 20, 20, 10, 30])
-        start_id = 5
-        expected_output = np.array([5, 6, 6, 5, 7])
-        output = make_labels_consecutive(labels, start_id)
+    @pytest.mark.parametrize("ignore_id", [-1, None])
+    @pytest.mark.parametrize("inplace", [True, False])
+    def test_make_labels_consecutive_empty(self, ignore_id: Optional[int], inplace: bool):
+        remapped_instance_ids, unique_instance_ids = make_labels_consecutive(
+            np.array([], dtype=np.int64), ignore_id=ignore_id, inplace=inplace, return_unique_labels=True
+        )
 
-        np.testing.assert_array_equal(output, expected_output)
+        assert len(remapped_instance_ids) == 0
+        assert len(unique_instance_ids) == 0
+
+    @pytest.mark.parametrize("ignore_id", [-1, None])
+    @pytest.mark.parametrize("inplace", [True, False])
+    def test_make_labels_consecutive_no_remapping_necessary(self, ignore_id: Optional[int], inplace: bool):
+        num_instances = 10
+        instance_ids = np.arange(num_instances, dtype=np.int64)
+
+        remapped_instance_ids, unique_instance_ids = make_labels_consecutive(
+            instance_ids, ignore_id=ignore_id, inplace=inplace, return_unique_labels=True
+        )
+
+        np.testing.assert_array_equal(instance_ids, remapped_instance_ids)
+        np.testing.assert_array_equal(instance_ids, unique_instance_ids)
+
+    @pytest.mark.parametrize("ignore_id", [-1, None])
+    @pytest.mark.parametrize("inplace", [True, False])
+    def test_make_labels_consecutive_remapping_necessary(self, ignore_id: Optional[int], inplace: bool):
+        offset = 5
+        num_instances = 10
+        instance_ids = np.concatenate(
+            [
+                np.arange(num_instances / 2, dtype=np.int64),
+                np.arange(num_instances / 2, dtype=np.int64),
+                np.arange(offset + num_instances / 2, offset + num_instances, dtype=np.int64),
+            ]
+        )
+
+        expected_remapped_instance_ids = np.concatenate(
+            [
+                np.arange(num_instances / 2, dtype=np.int64),
+                np.arange(num_instances / 2, dtype=np.int64),
+                np.arange(num_instances / 2, num_instances, dtype=np.int64),
+            ]
+        )
+        expected_unique_instance_ids = np.arange(num_instances, dtype=np.int64)
+
+        remapped_instance_ids, unique_instance_ids = make_labels_consecutive(
+            instance_ids, ignore_id=ignore_id, inplace=inplace, return_unique_labels=True
+        )
+
+        np.testing.assert_array_equal(expected_remapped_instance_ids, remapped_instance_ids)
+        np.testing.assert_array_equal(expected_unique_instance_ids, unique_instance_ids)
