697b5e35955d273af2ecb43c642f6a1875dbf16d
Josafat-Mattias Burmeister <33292321+josafatburmeister@users.noreply.github.com>
2025-03-31 11:15:47 +0200

    feat: add method to check whether flash attention is installed (#56)

--- src/pointtorch/config/_optional_dependencies.py
+++ src/pointtorch/config/_optional_dependencies.py
@@ -1,11 +1,27 @@
 """Utilities for configuring the package setup."""
 
-__all__ = ["open3d_is_available", "pytorch3d_is_available"]
+__all__ = ["open3d_is_available", "pytorch3d_is_available", "flash_attention_is_available"]
+
+
+def flash_attention_is_available():
+    """
+    Returns:
+        :code:`True` if FlashAttention is available and :code:`False` otherwise.
+    """
+    flash_attention_available = False
+    try:
+        import flash_attn as _  # type: ignore #pylint: disable=import-outside-toplevel
+
+        flash_attention_available = True
+    except (ModuleNotFoundError, TypeError):
+        pass
+
+    return flash_attention_available
 
 
 def open3d_is_available():
     """
-    Returns: `True` if Open3D is installed and `False` otherwise.
+    Returns: :code:`True` if Open3D is installed and :code:`False` otherwise.
     """
 
     open3d_available = False
@@ -21,7 +37,7 @@ def open3d_is_available():
 
 def pytorch3d_is_available() -> bool:
     """
-    Returns: `True` if PyTorch3D is installed and `False` otherwise.
+    Returns: :code:`True` if PyTorch3D is installed and :code:`False` otherwise.
     """
 
     pytorch3d_available = False
