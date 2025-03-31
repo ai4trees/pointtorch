77a6a288894cde929c35f564389ec3ab07699e85
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-03-31 10:58:06 +0200

    feat: add method to check whether flash attention is installed

--- test/config/test_config.py
+++ test/config/test_config.py
@@ -3,11 +3,20 @@
 import sys
 from types import ModuleType
 
-from pointtorch.config import open3d_is_available, pytorch3d_is_available
+from pointtorch.config import open3d_is_available, pytorch3d_is_available, flash_attention_is_available
 
 
 class TestConfig:
     """Tests for the pointtorch.config module."""
+
+    def test_flash_attention_is_available(self, monkeypatch):
+        monkeypatch.setitem(sys.modules, "flash_attn", ModuleType("flash_attn"))
+        assert flash_attention_is_available()
+
+    def test_flash_attention_not_available(self, monkeypatch):
+        monkeypatch.delitem(sys.modules, "flash_attn", raising=False)
+        monkeypatch.setattr("sys.path", [])
+        assert flash_attention_is_available() is False
 
     def test_open3d_available(self, monkeypatch):
         monkeypatch.setitem(sys.modules, "open3d", ModuleType("open3d"))
