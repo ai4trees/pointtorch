882e72e9986a1a2253fc0923e582511a899377fb
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-12-04 16:09:05 +0100

    update mypy stubs

--- mypy_stubs/torch_scatter/__init__.py
+++ mypy_stubs/torch_scatter/__init__.py
@@ -1,4 +1,4 @@
 """Mypy stubs for the torch-scatter package."""
 
 from .segment_csr import segment_csr
-from .scatter import scatter, scatter_max, scatter_min
+from .scatter import scatter, scatter_max, scatter_min, scatter_add
